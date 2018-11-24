326d6dfdae2e64465e1bf49d6afa73a08be1d015
Ciaran Bent [K0221230] <k00221230@student.lit.ie>
2018-11-24 23:03:02 +0000

    Updated solution
    +Added commenting template to all functions
    *Changed 'compile.bat' environment

--- programFiles/compile.bat
+++ programFiles/compile.bat
@@ -1,8 +1,16 @@
-::
-::
+::	@author			Ciaran Bent [K00221230]
+::	@creationDate	2018/24/11
+::	@description	...
 
-set CLASS_PATH=".;utils/algs4.jar;utils/stdlib.jar"
+@set JAVAC="C:\Program Files\Java\jdk1.8.0_172\bin\javac.exe"
+@set CLASS_PATH=".;utils/algs4.jar;utils/stdlib.jar"
 
-javac -cp %CLASS_PATH% Solver.java
+%JAVAC% -cp %CLASS_PATH% Solver.java
 
-pause
+@if not %errorlevel% == 0 (
+	echo There was an error.
+)
+
+java Solver < "puzzles\puzzle_01.txt"
+
+::pause
