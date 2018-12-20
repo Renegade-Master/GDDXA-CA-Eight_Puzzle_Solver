35decfe4b3d1fbe21241b82ba0313db0fbd78c35
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-12-20 18:45:13 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - Compilation file for when no IDE is available.
    ---
    #### Changes
    - N/A
    ---
    #### Removed
    - N/A

---
+++ EightPuzzle/compileAndRun.bat
@@ -0,0 +1,5 @@
+@set java="C:\Programming\Libraries\Java\jdk-11.0.1\bin\java.exe"
+@%java% "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\lib\idea_rt.jar=4092:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\bin" -Dfile.encoding=UTF-8 -classpath D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\out\production\EightPuzzle;D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\utils\algs4.jar com.gdd3a.sttAlgAi.eightPuzzle.Solver puzzle_01
+
+@echo.
+@pause
