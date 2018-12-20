1a440f9ac94338f4f3c2bb2dfab0297679c54a98
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2018-12-20 19:11:35 +0000

    Updated Solution
    ### Since Last Commit
    ---
    ---
    #### Additions
    - N/A
    ---
    #### Changes
    - Compilation batch file
    ---
    #### Removed
    - N/A

--- EightPuzzle/compileAndRun.bat
+++ EightPuzzle/compileAndRun.bat
@@ -1,5 +1,12 @@
-@set java="C:\Programming\Libraries\Java\jdk-11.0.1\bin\java.exe"
-@%java% "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\lib\idea_rt.jar=4092:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\bin" -Dfile.encoding=UTF-8 -classpath D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\out\production\EightPuzzle;D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\utils\algs4.jar com.gdd3a.sttAlgAi.eightPuzzle.Solver puzzle_01
+@set java=C:\Programming\Libraries\Java\jdk-11.0.1\bin\java.exe
+@set javaagent="-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\lib\idea_rt.jar=4092:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\bin"
+@set dfile="-Dfile.encoding=UTF-8"
+@set classpath="-classpath D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\out\production\EightPuzzle;D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\utils\algs4.jar"
+@set package=com.gdd3a.sttAlgAi.eightPuzzle.Solver
+
+@%java% %javaagent% %dfile% %classpath% %package% puzzle_01
+rem %java% "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\lib\idea_rt.jar=4092:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\bin" -Dfile.encoding=UTF-8 -classpath D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\out\production\EightPuzzle;D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\utils\algs4.jar com.gdd3a.sttAlgAi.eightPuzzle.Solver puzzle_01
+rem C:\Programming\Libraries\Java\jdk-11.0.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\lib\idea_rt.jar=4092:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition\2018.3.2\bin" -Dfile.encoding=UTF-8 -classpath D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\out\production\EightPuzzle;D:\_Projects\Repositories\GitKraken\College\Year3\AlgorithmsAndAI\3Y_AlgAI_Assignment_02\EightPuzzle\utils\algs4.jar com.gdd3a.sttAlgAi.eightPuzzle.Solver puzzle_01
 
 @echo.
 @pause
